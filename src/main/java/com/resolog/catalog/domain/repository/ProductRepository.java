24356d4e0078105a0a131604887a3c74b58b4262
scristia <scristia@student.codam.nl>
2026-04-18 18:13:55 +0100

    feat: add ProductRepository

---
+++ src/main/java/com/resolog/catalog/domain/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.resolog.catalog.domain.repository;
+
+import com.resolog.catalog.domain.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
