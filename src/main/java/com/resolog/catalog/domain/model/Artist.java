b34ec13c4aa4560f8c2a2ae0f0b034aeb8c0f63b
scristia <scristia@student.codam.nl>
2026-04-18 18:11:14 +0100

    feat: update Artist
    - solidified factory pattern for updating field
    - added auditing mechanism on Artist for created/updated data
    - default constructor is available at package level
    - artist is compared by id to avoid update invalidation

--- src/main/java/com/resolog/catalog/domain/model/Artist.java
+++ src/main/java/com/resolog/catalog/domain/model/Artist.java
@@ -6,29 +6,57 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import java.util.UUID;
-
 @Entity
-@Table(name = "artists")
-@Data
-@NoArgsConstructor
-public class Artist {
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = DbSchema.Artists.TABLE)
+public class Artist extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
     private UUID id;
+
     @Column(nullable = false)
     private String name;
+
     private String label;
+
     private String biography;
 
     public static Artist create(@NonNull String name) {
         Artist artist = new Artist();
-        artist.name = name;
+        artist.updateName(name);
         return artist;
+    }
+
+    public void updateName(@NonNull String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Artist name cannot be blank");
+        }
+        this.name = name;
+    }
+
+    public void updateLabel(@NonNull String label) {
+        if (label.isBlank()) {
+            throw new IllegalArgumentException("Artist label cannot be blank");
+        }
+        this.label = label;
+    }
+
+    public void updateBiography(@NonNull String biography) {
+        if (biography.isBlank()) {
+            throw new IllegalArgumentException("Artist biography cannot be blank");
+        }
+        this.biography = biography;
     }
 }
