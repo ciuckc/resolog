bd042146c813039a0a1bf46215bdcd37fa6a582b
scristia <scristia@student.codam.nl>
2026-04-20 01:55:33 +0200

    fix: remove start import on Artist

--- src/main/java/com/resolog/catalog/domain/model/Artist.java
+++ src/main/java/com/resolog/catalog/domain/model/Artist.java
@@ -1,13 +1,19 @@
 package com.resolog.catalog.domain.model;
 
-import jakarta.persistence.*;
-
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+
+import java.util.UUID;
 
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
