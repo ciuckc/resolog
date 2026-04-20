117aa1bd512c723c514d2a5c726d13934191f670
scristia <scristia@student.codam.nl>
2026-04-20 02:02:41 +0200

    feat: add query for testing if artist belongs to any product

--- src/main/java/com/resolog/catalog/domain/repository/ProductRepository.java
+++ src/main/java/com/resolog/catalog/domain/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.resolog.catalog.domain.repository;
 
 import com.resolog.catalog.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+
+    boolean existsByArtistsId(UUID artistId);
 }
